aa66d39384c61c09e96234290fd0b1a82c760329
JC <jorge.e.colindres@gmail.com>
2014-06-03 20:56:14 -0400

    Add additional middleware

--- config/express.js
+++ config/express.js
@@ -3,7 +3,8 @@
 var express = require('express');
 
 module.exports = function(db) {
-  var app = express();
+  var app = express(),
+      config = require('./config');
 
   app.locals.title = config.app.title;
 
@@ -13,16 +14,29 @@ module.exports = function(db) {
   app.use(require('body-parser')());
   app.use(require('method-override')());
   app.use(require('cookie-parser')());
+  app.use(require('connect-flash')());
+  app.use(require('express-validator')());
 
   if (process.env.NODE_ENV === 'development') {
     app.use(require('morgan')('dev'));
+    app.use(require('connect-livereload')());
+    app.use(require('errorhandler')());
+
     app.set('view cache', false);
     app.set('showStackError', true);
   }
 
-   // Simulate DELETE and POST
-  app.use(require('method-override')());
-
   // Enable jsonp
   app.enable('jsonp callback');
+
+  // Set the views path
+  app.set('views', config.root + '/server/views');
+
+  // Pretty HTML
+  app.locals.pretty = true;
+
+  // Load up static assets
+  app.use(require('serve-favicon')(config.root + '/public/favicon.ico'));
+  app.use('/public', express.static(config.root + '/public'));
+
 };
