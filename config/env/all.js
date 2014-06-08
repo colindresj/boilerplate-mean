12d66bf9718f1a65b3662450ba25f4569e882570
JC <jorge.e.colindres@gmail.com>
2014-06-08 14:46:52 -0400

    Use port 8000 as the default port

--- config/env/all.js
+++ config/env/all.js
@@ -4,7 +4,7 @@ var rootPath = require('path').normalize(__dirname + '/../..');
 
 module.exports = {
   root: rootPath,
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 8000,
   templateEngine: 'swig',
   app: {
     title: 'Boilerplate MEAN'
