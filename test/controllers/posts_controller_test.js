f9e18da331e5c5b211925e563be725df46d89379
JC <jorge.e.colindres@gmail.com>
2014-09-23 23:58:49 -0400

    Set up intial spec format

---
+++ test/controllers/posts_controller_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect,
+    supertest = require('supertest');
+
+describe('some test', function () {
+  it('should assert something', function () {
+    expect(true).to.be.true;
+  });
+});
