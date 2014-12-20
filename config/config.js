3d6f611e60cee78a02d707df80b8936195c5a72f
JC <jorge.e.colindres@gmail.com>
2014-12-20 12:12:33 -0500

    Fix loading env configs

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
 var _ = require('lodash'),
     fs = require('fs'),
     config = require('./env/all'),
-    envConfig = require('./env/' + process.env.NODE_ENV) || {};
+    envConfig;
 
 // Check if the current node env exists and is set to one of our configs
 // and if not set it to development by default
@@ -12,4 +12,5 @@ process.env.NODE_ENV = ~fs.readdirSync('./config/env').map(function(file) {
 }).indexOf(process.env.NODE_ENV) > 0 ? process.env.NODE_ENV : 'development';
 
 // Extend the base configuration and environment specific config
+envConfig = require('./env/' + process.env.NODE_ENV) || {};
 module.exports = _.merge(config, envConfig);
