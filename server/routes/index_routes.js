277ce64222e62754f9bcdaa7bdde48b4f180594f
JC <jorge.e.colindres@gmail.com>
2014-12-20 14:26:54 -0500

    Use express.router for index routes

--- server/routes/index_routes.js
+++ server/routes/index_routes.js
@@ -1,8 +1,15 @@
 'use strict';
 
+var express = require('express'),
+var express = require('express');
 module.exports = function(app) {
   var index = require(__dirname + '/../controllers/index_controller');
+  var indexRouter = express.Router(),
+      indexController = require(__dirname + '/../controllers/index_controller');
 
   app.route('/')
      .get(index.show);
+  indexRouter.route('/')
+    .get(indexController.show);
+  app.use('/', indexRouter);
 };
