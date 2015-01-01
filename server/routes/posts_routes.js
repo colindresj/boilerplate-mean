7ee387b66e084c867a2f2717f1d8f6e8a237e50f
JC <jorge.e.colindres@gmail.com>
2015-01-01 11:44:10 -0500

    Directly scope resource routes to the resource

--- server/routes/posts_routes.js
+++ server/routes/posts_routes.js
@@ -6,14 +6,14 @@ module.exports = function(app) {
   var postsRouter = express.Router(),
       postsController = require(__dirname + '/../controllers/posts_controller');
 
-  postsRouter.route('/posts')
+  postsRouter.route('/')
     .get(postsController.index)
     .post(postsController.create);
 
-  postsRouter.route('/posts/:id')
+  postsRouter.route('/:id')
     .get(postsController.show)
     .put(postsController.update)
     .delete(postsController.destroy);
 
-  app.use('/api/v1', postsRouter);
+  app.use('/api/v1/posts', postsRouter);
 };
