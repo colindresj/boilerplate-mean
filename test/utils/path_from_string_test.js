dd73d55c3717799efee6ab1b1ef9a1d039e175c6
JC <jorge.e.colindres@gmail.com>
2015-01-08 21:22:40 -0500

    Add test coverage around path from string module

---
+++ test/utils/path_from_string_test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var expect = require('chai').expect,
+    sinon = require('sinon'),
+    pathFromString = require('../../lib/utils/path_from_string'),
+    spy = sinon.spy();
+
+describe('#pathFromString', function() {
+  var fixture = buildFixture();
+
+  describe('when the final object is a function', function() {
+    var finalObj = pathFromString(fixture, 'some.obj.action');
+
+    it('returns the final object', function() {
+      expect(finalObj).to.be.ok;
+    });
+
+    it('keeps context in the final object', function() {
+      finalObj();
+
+      expect(spy).to.be.instanceof(Function)
+        .and.to.be.ok
+        .and.to.have.been.called;
+      expect(spy.thisValues).to.include(fixture.some.obj);
+    });
+  });
+
+  describe('when the final object is not a function', function() {
+    var finalObj = pathFromString(fixture, 'some.obj');
+
+    it('returns the final object', function() {
+      expect(finalObj).to.be.instanceof(Object)
+        .and.to.be.ok;
+    });
+  });
+});
+
+function buildFixture() {
+  return {
+    some: {
+      obj: {
+        action: spy
+      }
+    }
+  }
+}
